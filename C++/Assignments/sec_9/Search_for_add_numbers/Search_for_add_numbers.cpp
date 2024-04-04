923a40a8fc528f2af2a0401bad518b3c542f6075
Mohamed Ahmed Hussein <mohamedahmed_DK@hotmail.com>
2024-04-04 19:11:14 +0200

    add function for Search_for_add_numbers file

--- C++/Assignments/sec_9/Search_for_add_numbers/Search_for_add_numbers.cpp
+++ C++/Assignments/sec_9/Search_for_add_numbers/Search_for_add_numbers.cpp
@@ -1,23 +1,26 @@
 #include "inc/search.hpp"
 
+void getUserVector(std::vector<int>& user_vector,int& target_number);
 
 int main(void)
 {
-    std::vector<int> test = {3,2,4};
-    int target = 6;
+    std::vector<int> user_vector;
+    int target_number;
+    
+    getUserVector(user_vector,target_number);
 
-    auto escape_element = [&test](std::vector<int> &saved_vector, int remove_location)
+    auto escape_element = [&user_vector](std::vector<int> &saved_vector, int remove_location)
     {
         saved_vector.clear();
-        saved_vector=test;
+        saved_vector=user_vector;
         saved_vector[remove_location]=0;
     };
 
-    for (int index_counter = 0; index_counter < test.size(); index_counter++)
+    for (int index_counter = 0; index_counter < user_vector.size(); index_counter++)
     {
         std::vector<int> tem;
         int location_founded;
-        unsigned int search_number = (unsigned int)(target - test[index_counter]);
+        unsigned int search_number = (unsigned int)(target_number - user_vector[index_counter]);
         escape_element(tem, index_counter);
         search_result_t search_result = Search(tem, search_number, location_founded);
         if (search_result == Found)
@@ -31,4 +34,20 @@ int main(void)
     }
 
     return 0;
+}
+void getUserVector(std::vector<int>& user_vector,int& target_number) {
+    int input;
+
+    std::cout << "Enter integers for the vector (enter -1 to finish): ";
+
+    while (true) {
+        std::cin >> input;
+        if (input == -1) {
+            break;
+        }
+        user_vector.push_back(input);
+    }
+    std::cout <<std::endl<<"Enter the target number: ";
+    std::cin >> target_number;
+
 }
